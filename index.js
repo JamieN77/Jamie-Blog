5c13d98b310149c14e1dcdbdb93c1692cb72e0a0
JamieN77 <yuqiye093@gmail.com>
2023-12-20 23:08:33 -0500

    Separated API and server. Applied postgresSQL

--- index.js
+++ index.js
@@ -1,20 +1,136 @@
 import express from "express";
 import bodyParser from "body-parser";
-import postRoutes from "./routes/postRoutes.js"; // Adjust the path as necessary
+import multer from "multer";
+import path from "path";
+import pg from "pg";
 
 const app = express();
-const port = 5000;
+const port = 4000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "blogdb",
+  password: "2627897pppp",
+  port: 5432,
+});
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// Use routes from postRoutes
-app.use("/", postRoutes);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "userimg/");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
 
-// Static files middleware
-app.use(express.static("public"));
-app.use("/userimg", express.static("userimg"));
+const upload = multer({ storage: storage });
+const options = { day: "2-digit", month: "short", year: "numeric" };
 
-// Start the server
+// API routes
+// Example: Get all posts
+app.get("/posts", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM posts");
+    res.json(result.rows);
+  } catch (err) {
+    console.error("Error executing query", err.stack);
+    res.status(500).json({ message: "Error fetching posts." });
+  }
+});
+
+// Example: Get a single post
+app.get("/posts/:id", async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const result = await db.query("SELECT * FROM posts WHERE id = $1", [
+      postId,
+    ]);
+    if (result.rows.length > 0) {
+      res.json(result.rows[0]);
+    } else {
+      res.status(404).json({ message: "Post Not Found!" });
+    }
+  } catch (err) {
+    console.error("Error executing query", err.stack);
+    res.status(500).json({ message: "Error fetching post." });
+  }
+});
+
+// Example: Create a new post
+app.post("/posts", upload.single("image"), async (req, res) => {
+  const date = new Date();
+  const formattedDate = date.toLocaleDateString("en-GB", options);
+  let imagePath = req.file
+    ? req.file.path.replace(/\\/g, "/")
+    : "/images/i1.jpg";
+  try {
+    const newPost = await db.query(
+      "INSERT INTO posts (title, content, date, imagePath) VALUES ($1, $2, $3, $4) RETURNING *",
+      [req.body.title, req.body.content, formattedDate, imagePath]
+    );
+    res.status(201).json(newPost.rows[0]);
+  } catch (err) {
+    console.error("Error executing query", err.stack);
+    res.status(500).json({ message: "Error creating new post." });
+  }
+});
+
+// Example: Update a post
+app.put("/posts/:id", upload.single("image"), async (req, res) => {
+  const postId = req.params.id;
+  const imagePath = req.file ? req.file.path.replace(/\\/g, "/") : null;
+  const date = new Date();
+  const formattedDate = date.toLocaleDateString("en-GB", options);
+
+  try {
+    const updateQuery = {
+      text:
+        "UPDATE posts SET title = $1, content = $2, date = $3" +
+        (imagePath ? ", imagePath = $4" : "") +
+        " WHERE id = $5 RETURNING *",
+      values: imagePath
+        ? [req.body.title, req.body.content, formattedDate, imagePath, postId]
+        : [req.body.title, req.body.content, formattedDate, postId],
+    };
+
+    const updatedPost = await db.query(updateQuery);
+    if (updatedPost.rows.length === 0) {
+      return res.status(404).json({ message: "Post Not Found!" });
+    }
+    res.json(updatedPost.rows[0]);
+  } catch (err) {
+    console.error("Error executing query", err.stack);
+    res.status(500).json({ message: "Error updating post." });
+  }
+});
+
+// Example: Delete a post
+app.delete("/posts/:id", async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const deleteQuery = await db.query(
+      "DELETE FROM posts WHERE id = $1 RETURNING *",
+      [postId]
+    );
+    if (deleteQuery.rowCount === 0) {
+      return res.status(404).json({ message: "Post Not Found!" });
+    }
+    res.status(204).send();
+  } catch (err) {
+    console.error("Error executing query", err.stack);
+    res.status(500).json({ message: "Error deleting post." });
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Server started at port ${port}`);
+  console.log(`API is running at http://localhost:${port}`);
 });
---
+++ server.js
@@ -0,0 +1,110 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const app = express();
+const port = 3000;
+const API_BASE_URL = "http://localhost:4000";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "userimg/");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+const upload = multer({ storage: storage });
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/posts`);
+    res.render("index.ejs", { posts: response.data });
+  } catch (error) {
+    res.status(500).send("Error fetching posts");
+  }
+});
+
+app.get("/create", (req, res) => {
+  res.render("create.ejs");
+});
+
+app.get("/edit/:id", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/posts/${req.params.id}`);
+    res.render("edit.ejs", { post: response.data, postId: req.params.id });
+  } catch (error) {
+    res.status(500).send("Error fetching post");
+  }
+});
+
+app.get("/article/:id", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/posts/${req.params.id}`);
+    res.render("article.ejs", { post: response.data, postId: req.params.id });
+  } catch (error) {
+    res.status(500).send("Error fetching post");
+  }
+});
+
+// POST route for creating a new post
+app.post("/create", upload.single("image"), async (req, res) => {
+  try {
+    const formData = {
+      title: req.body.title,
+      content: req.body.content,
+      image: req.file ? req.file.filename : undefined,
+    };
+    await axios.post(`${API_BASE_URL}/posts`, formData);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).send("Error creating post");
+  }
+});
+
+// PUT route for updating a post
+app.post("/edit/:id", upload.single("image"), async (req, res) => {
+  try {
+    const formData = {
+      title: req.body.title,
+      content: req.body.content,
+      image: req.file ? req.file.filename : undefined,
+    };
+    await axios.put(`${API_BASE_URL}/posts/${req.params.id}`, formData);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).send("Error updating post");
+  }
+});
+
+// DELETE route for deleting a post
+app.post("/delete/:id", async (req, res) => {
+  try {
+    await axios.delete(`${API_BASE_URL}/posts/${req.params.id}`);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).send("Error deleting post");
+  }
+});
+
+app.use(express.static("public"));
+app.use("/userimg", express.static("userimg"));
+
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
